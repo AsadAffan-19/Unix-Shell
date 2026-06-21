be984acccd45c7c681b244f4d39de2b498499475
Affan Asad <asad@Asads-MacBook-Air.local>
2026-06-21 11:50:03 +0530

    [path]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.io.File;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
+
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
@@ -17,16 +19,42 @@ public class Main {
             } else if (input.startsWith("type ")) {
                 String command = input.substring(5).trim();
 
-                if ( command.equals("echo") || command.equals("exit") || command.equals("type")){
+                if (command.equals("echo") || command.equals("exit") || command.equals("type")) {
                     System.out.println(command + " is a shell builtin");
+
                 } else {
-                    System.out.println(command + ": not found");
+                    String path = System.getenv("PATH");
+                    // System.out.println(path); // add here
+
+                    String[] dirs = path.split(":");
+
+                    boolean found = false;
+
+                    for (String dir : dirs) {
+                        File file = new File(dir, command);
+
+                        boolean exist = file.exists();
+                        boolean executable = file.canExecute();
+
+                        if (exist && executable) {
+                            System.out.println(command + " is " + dir);
+                            found = true;
+                            break;
+
+                        }
+
+                    }
+
+                    if (!found) {
+                        System.out.println(command + ": not found");
+                    }
+
                 }
             } else {
                 System.out.println(input + ": command not found");
+
             }
 
-            
         }
     }
 }
